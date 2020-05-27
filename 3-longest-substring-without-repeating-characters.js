9d7ca2c74e54f54602a13760385be4bf7d215095
xmflswood <774898896@qq.com>
2020-05-27 15:48:42 +0800

    322

---
+++ 3-longest-substring-without-repeating-characters.js
@@ -0,0 +1,14 @@
+var lengthOfLongestSubstring = function(s) {
+    let str = ''
+    let size = 0
+    for (let i = 0; i < s.length; i++) {
+        let index = str.indexOf(s[i])
+        if (index === -1) {
+            str += s[i]
+            size = str.length > size ? str.length : size
+        } else {
+            str = str.substring(index + 1) + s[i]
+        }
+    }
+    return size
+};
