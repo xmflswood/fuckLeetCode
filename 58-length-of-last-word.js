b8cc4500d42d018d314ae7760637a8adc6b8302b
xmflswood <774898896@qq.com>
2020-06-02 20:54:07 +0800

    58 611

---
+++ 58-length-of-last-word.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+    let l = 0
+    for (let i = s.length - 1; i >= 0; i--) {
+        if (!s[i]) break
+        if (s[i] === ' ' && l === 0) continue
+        if (s[i] === ' ') return l
+        l++
+    }
+    return l
+};
