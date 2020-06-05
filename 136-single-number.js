6d0eda2ca5a777aaad48dc80a242540cc67e759b
xmflswood <774898896@qq.com>
2020-06-06 00:04:30 +0800

    yihuo

--- 136-single-number.js
+++ 136-single-number.js
@@ -3,7 +3,12 @@
  * @param {number[]} nums
  * @return {number}
  */
+// 
 var singleNumber = function(nums) {
-
+    let r = 0
+    for (let i = 0; i < nums.length; i++) {
+        r ^= nums[i]
+    }
+    return r
 };
 // why am i so stupid
