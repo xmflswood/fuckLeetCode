fd5b6bb880fad82b31fd2c38e2ded64ec1f8a225
xmflswood <774898896@qq.com>
2020-06-06 00:47:44 +0800

    268

--- 268-missing-number.js
+++ 268-missing-number.js
@@ -4,7 +4,7 @@
  * @return {number}
  */
 var missingNumber = function(nums) {
-    let sum = nums.length * (nums.length) / 2
+    let sum = nums.length * (nums.length + 1) / 2
     let sum2 = 0
     for (let i = 0; i < nums.length; i++) {
         sum2 += nums[i]
