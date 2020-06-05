ff292491bf45956e7eb068d69cf0f5af09198e0f
xmflswood <774898896@qq.com>
2020-06-06 00:47:20 +0800

    268

---
+++ 268-missing-number.js
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/missing-number/
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+    let sum = nums.length * (nums.length) / 2
+    let sum2 = 0
+    for (let i = 0; i < nums.length; i++) {
+        sum2 += nums[i]
+    }
+    return sum - sum2
+};
